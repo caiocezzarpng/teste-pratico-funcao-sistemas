473d06d79d23582d0ed2aeb16dc33bde45c90365
Caio Cesar <caiocezzarpng@gmail.com>
2024-06-22 19:12:47 -0300

    feat: Criando model do Beneficiario

---
+++ FI.WebAtividadeEntrevista/Models/BeneficiarioModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FI.WebAtividadeEntrevista.Models
+{
+    public class BeneficiarioModel
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// ClienteId
+        /// </summary>
+        public long ClienteId { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        [Required]
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Action (Register, Update, Remove)
+        /// </summary>
+        public string Action { get; set; }
+    }
+}
